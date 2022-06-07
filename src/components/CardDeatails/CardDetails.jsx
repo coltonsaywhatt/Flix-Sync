e6f666be64294e91ca8cb1809a786c4f22ee932a
coltonsaywhatt <coltonsaywhatt@gmail.com>
2022-06-06 23:40:46 -0400

    created card details component

---
+++ src/components/CardDeatails/CardDetails.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function MovieDetails() {
+  return (
+
+    <div class="movie_card" id="container">
+    <div class="info_section">
+      <div class="movie_header">
+        <img class="poster" src={`https://image.tmdb.org/t/p/original${props.poster_path}`}/>
+        <h1>{props.title}</h1>
+        <h4>{props.release_date}</h4><span class="minutes">R</span> <span class="minutes">117 min</span>
+        <p class="type">Action, Crime, Fantasy</p>
+      </div>
+      <div class="movie_desc">
+        <p class="text">Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work with an Orc to find a weapon everyone is prepared to kill for.</p>
+      </div>
+      <div class="movie_social">
+        <ul>
+          <li><button><i class="fa-solid fa-bookmark"></i>&nbsp; Add to watch list</button></li>
+          <li><button><i class="fa-solid fa-circle-check"></i>&nbsp; Add to watched list</button></li>
+        </ul>
+      </div>
+    </div>
+    <div class="blur_back backdrop"><img alt="" src="https://occ-0-2433-448.1.nflxso.net/art/cd5c9/3e192edf2027c536e25bb5d3b6ac93ced77cd5c9.jpg"/></div>
+  </div>
+
+  )
+}
+
+export default MovieDetails
